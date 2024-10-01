b9777211ba97d821f077b7971884905e0b1f7e37
chinaaap <aaa.bbfive@gmail.com>
2024-10-01 15:34:44 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,5 @@ component-test1
 
 <x-tests.card title="Title1" content="Content1" :message="$message"/>
 <x-tests.card title="Title2" />
+<x-tests.card title="Title3" class="bg-red-300"/>
 </x-tests.app>
