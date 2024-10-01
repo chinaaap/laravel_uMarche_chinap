4d63af8cd0ed40509bb2c51fbf58491d0e6a7741
chinaaap <aaa.bbfive@gmail.com>
2024-10-01 15:17:55 +0900

    属性で設定

---
+++ resources/views/components/tests/card.blade.php
@@ -0,0 +1,5 @@
+<div class="border-2 shadow-md w-1/4 p-2">
+    <div>{{ $title }}</div>
+    <div>image</div>
+    <div>{{ $content }}</div>
+</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
     <x-slot name="header">header1</x-slot>
 component-test1
+
+<x-tests.card title="Title1" content="Content1" />
 </x-tests.app>
