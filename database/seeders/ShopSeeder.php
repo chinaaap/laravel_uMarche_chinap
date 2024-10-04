3230949683296c655bfd878ddea429dfbd145db0
chinaaap <aaa.bbfive@gmail.com>
2024-10-04 16:15:56 +0900

    Shop追加

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('shops')->insert([
+            [
+                'owner_id' => 1,
+                'name' => 'shop name',
+                'information' => 'shop info shop info shop info shop info',
+                'filename' => '',
+                'is_selling' => true
+            ],
+            [
+                'owner_id' => 2,
+                'name' => 'shop name',
+                'information' => 'shop info shop info shop info shop info',
+                'filename' => '',
+                'is_selling' => true
+            ],
+        ]);
+    }
+}
