e685ef6ed6095f9146c2fb1bf4010b39706c7020
chinaaap <aaa.bbfive@gmail.com>
2024-10-06 13:14:30 +0900

    serviceへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -51,22 +52,9 @@ class ShopController extends Controller
     {
         $imageFile = $request->image; //一時保存
         if(!is_null($imageFile) && $imageFile->isValid() ){
-
-        //リサイズなしの場合
-        // Storage::putFile('public/shops', $imageFile);
-        // Storage::putFileAs('public/' . $folderName . '/' , $file, $fileNameToStore );
-
-        //リサイズありの場合
-        $fileName = uniqid(rand().'_');
-        $extension = $imageFile->extension();
-        $fileNameToStore = $fileName. '.' . $extension;
-        $resizedImage = InterventionImage::make($imageFile)
-        ->resize(1920, 1080)->encode();
-
-        // dd($imageFile, $resizedImage);
-        Storage::put('public/shops/' . $fileNameToStore,
-        $resizedImage);
+        $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
+
         return redirect()->route('owner.shops.index');
     }
 }
