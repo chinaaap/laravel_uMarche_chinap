f079b973ee3275272cb55972f78332794c8ccd06
chinaaap <aaa.bbfive@gmail.com>
2024-10-06 11:36:11 +0900

    フォームリクエスト

--- resources/views/admin/owners/edit.blade.php
+++ resources/views/admin/owners/edit.blade.php
@@ -27,9 +27,10 @@
                                 </div>
                               </div>
                               <div class="p-2 w-1/2 mx-auto">
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                 <div class="relative">
                                   <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                  <input type="email" id="email" name="email" value="{{ $owner->email }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                  <input type="text" id="email" name="email" value="{{ $owner->email }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
                               </div>
                               <div class="p-2 w-1/2 mx-auto">
@@ -39,6 +40,7 @@
                                 </div>
                               </div>
                               <div class="p-2 w-1/2 mx-auto">
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
                                 <div class="relative">
                                   <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
                                   <input type="password" id="password" name="password" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
