a43825cf339e48f49a97f6850d19c80299e68e62
chinaaap <aaa.bbfive@gmail.com>
2024-10-07 14:54:33 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
-            ImageSeeder::class
+            ImageSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
