a8bce793ed67dd9c0142145ce13dab7e2c2a3be3
chinaaap <aaa.bbfive@gmail.com>
2024-10-07 13:55:49 +0900

    image delete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -137,7 +137,7 @@ class OwnersController extends Controller
         return redirect()
         ->route('admin.owners.index')
         ->with(['message' => 'オーナー情報を削除しました。',
-    'status' => 'alert' ]);
+        'status' => 'alert' ]);
     }
 
     public function expiredOwnerIndex(){
--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -8,6 +8,7 @@ use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -95,11 +96,20 @@ class ImageController extends Controller
         'status' => 'info']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $image = Image::findOrFail($id);
+        $filePath = 'public/products/' . $image->filename;
+
+        if(Storage::exists($filePath)){
+            Storage::delete($filePath);
+        }
+
+        Image::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像を削除しました。',
+        'status' => 'alert' ]);
     }
 }
